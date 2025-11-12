4a98ae6e4515194b65aa61615f58aa90fb8c4089
solomenkoelizaveta-hub <solomenko.elizaveta@gmail.com>
2025-11-12 13:29:33 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,7 +1,9 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { 'Content-Type': 'text/html' });
+  res.writeHead(200, { 
+    'Content-Type': 'text/html; charset=utf-8'  // ← ДОБАВЬТЕ charset=utf-8
+  });
   res.end(`
     <html>
       <body>
